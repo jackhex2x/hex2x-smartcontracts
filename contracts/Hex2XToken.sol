66bc856fc35d87726ae14027ec5992f26bdf0945
tech-somish <contact@somish.com>
2020-06-11 16:33:23 +0530

    updated burn tokens function

--- contracts/Hex2XToken.sol
+++ contracts/Hex2XToken.sol
@@ -19,8 +19,8 @@ contract Hex2XToken is ERC20Detailed, Ownable {
     /*
     * @dev Burn 'amount' tokens from caller address reducing totalSupply
     */
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
+    function burnFrom(address account, uint256 amount) public onlyOwner {
+        _burnFrom(account, amount);
     }
 
     /*
